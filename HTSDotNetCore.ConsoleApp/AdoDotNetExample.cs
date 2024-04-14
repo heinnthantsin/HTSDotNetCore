30bff6969cfce03b969645e4fc36d94103ddbe1b
heinnthantsin <heinnthant01@gmail.com>
2024-04-14 13:24:06 +0630

    Ado.net Create

---
+++ HTSDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,74 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+
+
+namespace HTSDotNetCore.ConsoleApp
+{
+    internal class AdoDotNetExample
+    {
+
+
+        private readonly SqlConnectionStringBuilder _sqlConnectionBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "nick", //server name
+            InitialCatalog = "DotNetBatch4", // database name
+            UserID = "sa",
+            Password = "sasa@123"
+        };
+        public void Read()
+        {   
+            SqlConnection connection = new SqlConnection(_sqlConnectionBuilder.ConnectionString);
+
+            connection.Open();
+            string query = "SELECT * FROM tbl_blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            connection.Close();
+
+            /*foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("blog_id" + dr[0]);
+                Console.WriteLine("blog_id" + dr[1]);
+                Console.WriteLine("blog_id" + dr[2]);
+                Console.WriteLine("blog_id" + dr[3]);
+                Console.WriteLine("---------------------");
+            }*/
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("blog_id" + dr["blogId"]);
+                Console.WriteLine("blog_id" + dr["blogTitle"]);
+                Console.WriteLine("blog_id" + dr["blogAuthor"]);
+                Console.WriteLine("blog_id" + dr["blogContent"]);
+                Console.WriteLine("---------------------");
+            }
+        }
+
+        public void Create(String title,String author, String content)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @"INSERT INTO [dbo].[tbl_blog]
+           ([blogTitle]
+           ,[blogAuthor]
+           ,[blogContent])
+     VALUES
+           (@blogTitle,
+			@blogAuthor,
+			@blogContent)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@blogTitle", title);
+            cmd.Parameters.AddWithValue("@blogAuthor", author);
+            cmd.Parameters.AddWithValue("@blogcontent", content);
+            int result = cmd.ExecuteNonQuery();
+
+            string msg = result > 0 ? "Blog created" : "Creation Failed";
+            Console.WriteLine(msg);
+            connection.Close();
+        }
+    }
+}
--- HTSDotNetCore.ConsoleApp/Program.cs
+++ HTSDotNetCore.ConsoleApp/Program.cs
@@ -1,39 +1,7 @@
-﻿using System.Data;
-using System.Data.SqlClient;
+﻿using HTSDotNetCore.ConsoleApp;
 
-Console.WriteLine("hello,world");
-
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "nick";
-stringBuilder.InitialCatalog = "DotNetBatch4";
-stringBuilder.UserID = "sa";
-stringBuilder.Password = "sasa@123";
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
-
-connection.Open();
-string query = "SELECT * FROM tbl_blog";
-SqlCommand cmd = new SqlCommand(query, connection);
-SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-DataTable dt = new DataTable();
-sqlDataAdapter.Fill(dt);
-connection.Close();
-
-/*foreach (DataRow dr in dt.Rows)
-{
-    Console.WriteLine("blog_id" + dr[0]);
-    Console.WriteLine("blog_id" + dr[1]);
-    Console.WriteLine("blog_id" + dr[2]);
-    Console.WriteLine("blog_id" + dr[3]);
-    Console.WriteLine("---------------------");
-}*/
-
-foreach (DataRow dr in dt.Rows)
-{
-    Console.WriteLine("blog_id" + dr["blogId"]);
-    Console.WriteLine("blog_id" + dr["blogTitle"]);
-    Console.WriteLine("blog_id" + dr["blogAuthor"]);
-    Console.WriteLine("blog_id" + dr["blogContent"]);
-    Console.WriteLine("---------------------");
-}
+AdoDotNetExample adoDotNet = new AdoDotNetExample();
+adoDotNet.Read();
+adoDotNet.Create("abc", "def", "hij");
 
 Console.ReadKey(true); 
